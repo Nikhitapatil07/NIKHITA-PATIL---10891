d9e328051a97d2448985ccde2411cfe867ea5254
Nikhitapatil07 <nikhitaa067@gmail.com>
2026-01-04 19:27:37 +0530

    1.c

--- NIKHITA - PFC/Practical Lab 1&2/1.c
+++ NIKHITA - PFC/Practical Lab 1&2/1.c
@@ -3,8 +3,9 @@
 int main()           // Main function starts
 {
     // Printing name and age
-    printf("My name is sarthak deshmukh\n");
+    printf("My name is Nikhita patil\n");
     printf("I am 18 years old\n");
     printf("Welcome to C Programming!\n");
     return 0;
 }
+
